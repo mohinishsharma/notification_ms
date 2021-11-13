dc92a333487454d3961c5ee01bbf42d9a687886e
Mohinish Sharma <sharmamohinish67@gmail.com>
2021-11-13 20:11:50 +0530

    Added ability to find dispatch fn in provider

--- dispatch/index.js
+++ dispatch/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const config = require('config');
+const path = require('path');
 /** @type {string[]} */
 const providers = config.get('providers');
 
@@ -12,16 +13,27 @@ function dispatchNotifications(notifications) {
   const dispatchers = providers.reduce((a,p) => {
     if (fs.existsSync( __dirname + '/' + p + '.dispatch.js')) {
       const dispatchFile = require(__dirname + '/' + p + '.dispatch');
-      if ('dispatch' in dispatchFile) a[p] = dispatchFile.dispatch;
+      if ('dispatch' in dispatchFile) a[p] = dispatchFile;
     }
     return a;
   }, {});
   const foundDispatcher = Object.keys(dispatchers);
   const missingDispatcher = notificationProviders.filter(np => !foundDispatcher.includes(np));
-  missingDispatcher.length && missingDispatcher.forEach(md => delete notifications[md]);
+  if (missingDispatcher.length) {
+    missingDispatcher.forEach(md => {
+      const providerModule = require(path.join(path.dirname(__dirname),'providers', md));
+      if ('dispatch' in providerModule) {
+        dispatchers[md] = providerModule;
+      } else {
+        delete notifications[md]
+      }
+    });
+    Object.keys(dispatchers).forEach(d => missingDispatcher.includes(d) && delete missingDispatcher[d]);
+  }
   missingDispatcher.length && console.log("[DISPATCHER] missing dispatcher for provider(s) \"" + missingDispatcher.join() + "\"");
   const providerName = Object.keys(notifications);
-  providerName.forEach(p => dispatchers[p](notifications[p]));
+  providerName.length && console.log("[DISPATCHER] Dispatched for \"" + providerName.join() + "\"");
+  providerName.forEach(p => dispatchers[p]['dispatch'](notifications[p]));
 }
 
 module.exports = dispatchNotifications;
